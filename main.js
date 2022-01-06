f7efbdd26b4654495270b340fd7a4849cc51d9de
MrAmazingCoder <78091082+AnushandRithish64@users.noreply.github.com>
2022-01-06 11:29:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+verified = 'false'
+rdmnmbr = Math.floor(100000 + Math.random() * 900000)
+
+ThunkableWebviewerExtension.postMessage(rdmnmbr)
+
+function Checkforcode() {
+
+  userincode = document.getElementById('code').value
+  if (userincode == rdmnmbr) {
+    window.open("videocll.html");
+    document.getElementById('result').innerHTML = 'Logging you in'
+  }
+  else
+    document.getElementById('result').innerHTML = 'Code Invalid'
+
+
+}
+function checkifverified() {
+
+
+  if (verified == "false") {
+
+    window.open("index.html", "_self");
+  }
+}
